5cc383bf42a5f72219900ae72264b442b48a7234
Lrakulka <olecsandr32@gmail.com>
2016-02-20 17:20:09 +0200

    Test of absoluteLayout

--- app/src/main/java/com/myandroid/mygorod/UnitsFragment.java
+++ app/src/main/java/com/myandroid/mygorod/UnitsFragment.java
@@ -33,8 +33,23 @@ public class UnitsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_units, container,false);
 
 
-        TextView textIdOgorod = (TextView)rootView.findViewById(R.id.textView);
+        TextView textIdOgorod = (TextView) rootView.findViewById(R.id.textView);
+        Button button1 = (Button) rootView.findViewById(R.id.button1);
+        Button button3 = (Button) rootView.findViewById(R.id.button3);
+        final Button button2 = (Button) rootView.findViewById(R.id.button2);
 
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button2.setX(button2.getX() + 10);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button2.setX(button2.getX() - 10);
+            }
+        });
         String textForSasha = "idOgorod = " + String.valueOf(ogorofMap);
         textIdOgorod.setText(textForSasha.toString());
         return rootView;
