ca34d4724480dc5c54f9c9d6ac7ceaab28b9bc37
Lrakulka <olecsandr32@gmail.com>
2016-02-20 17:45:54 +0200

    Add error Layout

--- app/src/main/java/com/myandroid/mygorod/fragments/UnitsFragment.java
+++ app/src/main/java/com/myandroid/mygorod/fragments/UnitsFragment.java
@@ -25,35 +25,15 @@ public class UnitsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+        View rootView;
         Intent intent = getActivity().getIntent();
         if (intent != null) {
             ogorofMap = (OgorodItem) intent.getSerializableExtra(getString(R.string.ogorod_key));
+            rootView = inflater.inflate(R.layout.fragment_units, container,false);
+        } else {
+            rootView = inflater.inflate(R.layout.error, container,false);
         }
-        Log.v(LOG_TAG,"idOgorod = " + ogorofMap);
-        View rootView = inflater.inflate(R.layout.fragment_units, container,false);
 
-
-        TextView textIdOgorod = (TextView) rootView.findViewById(R.id.textView);
-        Button button1 = (Button) rootView.findViewById(R.id.button1);
-        Button button3 = (Button) rootView.findViewById(R.id.button3);
-        final Button button2 = (Button) rootView.findViewById(R.id.button2);
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                button2.setX(button2.getX() + 10);
-            }
-        });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                button2.setX(button2.getX() - 10);
-            }
-        });
-        String textForSasha = "idOgorod = " + String.valueOf(ogorofMap);
-
-        textIdOgorod.setText(textForSasha.toString());
         return rootView;
     }
 
